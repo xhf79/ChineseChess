64965f25a5b335b49efaf482497ec49df010e38d
xhf79 <37611625+xhf79@users.noreply.github.com>
2021-04-16 14:17:17 +0800

    Update ChessBoardMain.java

--- com/pj/chess/ChessBoardMain.java
+++ com/pj/chess/ChessBoardMain.java
@@ -487,6 +487,7 @@ public class ChessBoardMain extends JFrame {
 		if (JOptionPane.showConfirmDialog(this, msg + "是否继续？", "信息",
 				 JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
 			dispose(); //dispose这个函数来自哪里？2021-04-14
+                        //继承自JFrame的，表示释放窗口资源。2021-04-16
 			new ChessBoardMain();
 		} else{
 			dispose();
